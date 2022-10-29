ea2ef8936c0f90c7454f6a9d46aa91fad78d127a
DenisVaschenko <denvoser@gmail.com>
2022-10-29 11:49:08 +0300

    test cases were created

---
+++ Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice3
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int sum = 0;
+            Console.WriteLine("sum of numbers equals " + sum);
+        }
+    }
+}
