fb105cf5440fe0f7668a794cc4d03ada824fab46
DenisVaschenko <denvoser@gmail.com>
2022-10-29 11:53:14 +0300

    Full implementation of the system was created

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -6,9 +6,25 @@ namespace Practice3
     {
         static void Main(string[] args)
         {
-            int n = 0;
+            /* case 1 n = 0
+             * case 2 n = 1
+             * case 3 n = 9
+             * case 4 n = 10
+             */
+
+            Console.WriteLine("Enter number n");
+            int n = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
             Console.WriteLine("sum of numbers equals " + sum);
+            /* case 1 sum =0
+             * case 2 sum = 1
+             * case 3 sum = 45
+             * case 4 sum = 55
+             */
         }
     }
 }
