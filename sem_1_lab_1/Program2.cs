835b1a7730319013730e59d73d4a116649dd9184
DenisVaschenko <denvoser@gmail.com>
2022-11-13 05:28:04 +0200

    Full implementation of program2 was created

--- sem_1_lab_1/Program2.cs
+++ sem_1_lab_1/Program2.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Data;
 
 namespace Program2
 {
     class Program
     {
+        static double root(int sqr_x)
+        {
+            double left = 0, right = sqr_x;
+            double x = 0;
+            while (Math.Abs(sqr_x - x*x) > 0.1)
+            {
+                x = (left +right) / 2;
+                if (x * x == sqr_x) { break; }
+                else if (x * x < sqr_x) { left = x; }
+                else { right = x; }
+            }
+            return x;
+        }
         static void Main(string[] args)
         {
             /*
@@ -15,7 +29,23 @@ namespace Program2
              * test case 5 n = -1
              */
             Console.WriteLine("Enter the number n");
-            int n = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool answer = true;
+            if (n > 1)
+            {
+                double root_n = root(n);
+                for (int i = 2; i <= root_n; i++)
+                {
+                    if (n % i == 0) { answer = false; break; }
+                }
+                Console.WriteLine(answer);
+            }
+            else
+            {
+                answer = false;
+                Console.WriteLine(answer);
+            }
+
             /*
              * test case 0 false
              * test case 1 true
