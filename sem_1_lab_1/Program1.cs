7741b85f5e93555205feb348d91c01fe6dea7e11
DenisVaschenko <denvoser@gmail.com>
2022-11-13 04:48:05 +0200

    Test cases for program1 were created

--- sem_1_lab_1/Program1.cs
+++ sem_1_lab_1/Program1.cs
@@ -6,6 +6,7 @@ namespace Program1
     {
         static void Main(string[] args)
         {
+            // case 0 x0 = 4.6; xn = 5.8; n = 6
             Console.WriteLine("Enter the number x0");
             double x0 = 0;
             Console.WriteLine("Enter the number xn");
@@ -14,6 +15,7 @@ namespace Program1
             double n = 0;
             double x = 0, y = 0;
             Console.WriteLine(x + " " + y);
+            // case 0 x= 4.6, 4.8, 5, 5.2, 5.4, 5.6, 5.8; y= 446.947, 530.609, 625.999, 730.164, 850.773, 984.376, 1132.162
         }
     }
 }
