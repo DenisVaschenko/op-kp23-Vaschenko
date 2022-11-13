8f8d257a9a20b02a4e77a1369901015949454cf7
DenisVaschenko <denvoser@gmail.com>
2022-11-13 04:39:12 +0200

    Prototype of the program1 was created

---
+++ sem_1_lab_1/Program1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number x0");
+            double x0 = 0;
+            Console.WriteLine("Enter the number xn");
+            double xn = 0;
+            Console.WriteLine("Enter the number n");
+            double n = 0;
+            double x = 0, y = 0;
+            Console.WriteLine(x + " " + y);
+        }
+    }
+}
