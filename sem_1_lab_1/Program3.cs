81f0c87ae0c39a88a9c98fd555f499f940c72b40
DenisVaschenko <denvoser@gmail.com>
2022-11-13 06:02:27 +0200

    Full implementation of program3 was created

--- sem_1_lab_1/Program3.cs
+++ sem_1_lab_1/Program3.cs
@@ -19,8 +19,23 @@ namespace Program3
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the number x");
             int x = Convert.ToInt32(Console.ReadLine());
-            int fact_n = 0, powered_x = 0;
-            Console.WriteLine("factorial of number = " + fact_n);
+            int fact_n = 1, powered_x = 1;
+            if (n >= 0)
+            {
+                for (int i = 2; i<=n; i++)
+                {
+                    fact_n *= i;
+                }
+                Console.WriteLine("factorial of number = " + fact_n);
+            }
+            else
+            {
+                Console.WriteLine("Unable to calculate factorial of number n");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                powered_x *= x;
+            }
             Console.WriteLine("Powered number = " + powered_x);
         }
         /*
