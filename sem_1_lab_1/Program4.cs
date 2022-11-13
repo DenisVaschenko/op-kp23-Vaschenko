2707e4a147e8fd957b135816468dab56a734b432
DenisVaschenko <denvoser@gmail.com>
2022-11-13 06:16:51 +0200

    Protorype of program4 was created

---
+++ sem_1_lab_1/Program4.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+
+namespace Program4
+{
+    class Program
+    {
+        static double sin(double x)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number x");
+            for (int i = 0; i<10; i++)
+            {
+                double x = 0;
+                Console.WriteLine(x);
+                Console.WriteLine("Result matches with Math.Sin()");
+            }
+        }
+    }
+}
