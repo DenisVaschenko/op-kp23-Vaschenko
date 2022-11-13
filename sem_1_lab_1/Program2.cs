3dee1e6bc1220c2e326c8eb46913ae76fd74bf66
DenisVaschenko <denvoser@gmail.com>
2022-11-13 05:13:51 +0200

    Test cases for program2 were created

--- sem_1_lab_1/Program2.cs
+++ sem_1_lab_1/Program2.cs
@@ -6,8 +6,24 @@ namespace Program2
     {
         static void Main(string[] args)
         {
+            /*
+             * test case 0 n = 1
+             * test case 1 n = 2
+             * test case 2 n = 3
+             * test case 3 n = 4
+             * test case 4 n = 5
+             * test case 5 n = -1
+             */
             Console.WriteLine("Enter the number n");
             int n = 0;
+            /*
+             * test case 0 false
+             * test case 1 true
+             * test case 2 true
+             * test case 3 false
+             * test case 4 true
+             * test case 5 false
+             */
         }
     }
 }
