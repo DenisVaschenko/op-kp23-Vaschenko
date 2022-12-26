d059e6241305ff5346e97d17d8d750f49476cac8
DenisVaschenko <denvoser@gmail.com>
2022-12-26 10:12:27 +0200

    Full implementation of the program was created

--- sem_1_lab_2/Program.cs
+++ sem_1_lab_2/Program.cs
@@ -7,46 +7,124 @@ namespace Program1
 {
     class Percolation
     {
+        static int[,] arr;
+        static int[] nodes;
+        static int num_of_opened, length;
         // creates n-by-n grid, with all sites initially blocked
         static void init(int n)
         {
+            arr = new int[n + 1, n + 1];
+            num_of_opened = 0;
+            length = n + 1;
+            nodes = new int[n * n + 2];
+            for (int i = 1; i < n * n + 2; i++)
+            {
+                nodes[i] = i;
+            }
         }
 
         // opens the site (row, col) if it is not open already
         static void open(int row, int col)
         {
+            if (arr[row, col] == 0)
+            {
+                arr[row, col] = 1;
+                int index = getIndex(row, col);
+                if (row > 1 && arr[row - 1, col] != 0)
+                {
+                    union(index, getIndex(row - 1, col));
+                }
+                else if (row == 1)
+                {
+                    union(index, 0);
+                }
+                if (row < length - 1 && arr[row + 1, col] != 0)
+                {
+                    union(index, getIndex(row + 1, col));
+                }
+                else if (row == length - 1)
+                {
+                    union(index, nodes.Length - 1);
+                }
+                if (col > 1 && arr[row, col - 1] != 0)
+                {
+                    union(index, getIndex(row, col - 1));
+                }
+                if (col < length - 1 && arr[row, col + 1] != 0)
+                {
+                    union(index, getIndex(row, col + 1));
+                }
+                num_of_opened++;
+            }
         }
 
         // is the site (row, col) open?
         static bool isOpen(int row, int col)
         {
+            return arr[row, col] != 0;
         }
 
         // is the site (row, col) full?
         static bool isFull(int row, int col)
         {
+            if (nodes[getIndex(row, col)] == 0)
+            {
+                arr[row, col] = 2;
+            }
+            return arr[row, col] == 2;
         }
 
         // returns the number of open sites
         static int numberOfOpenSites()
         {
+            return num_of_opened;
         }
 
         // does the system percolate?
         static bool percolates()
         {
+            return nodes[nodes.Length - 1] == 0;
         }
 
         // prints the matrix on the screen
         // The method should display different types of sites (open, blocked, full)
         static void print()
         {
+            for (int i = 1; i < length; i++)
+            {
+                for (int j = 1; j < length; j++)
+                {
+                    if (nodes[getIndex(i, j)] == 0)
+                    {
+                        arr[i, j] = 2;
+                    }
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
         static void union(int x, int y)
         {
+            int ComponentX = nodes[x];
+            int ComponentY = nodes[y];
+            if (ComponentX == 0)
+            {
+                int tmp = ComponentY;
+                ComponentY = ComponentX;
+                ComponentX = tmp;
+            }
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i] == ComponentX)
+                {
+                    nodes[i] = ComponentY;
+                }
+            }
+
         }
         static int getIndex(int row, int col)
         {
+            return (row - 1) * (length - 1) + col;
         }
         static bool initTest()
         {
@@ -264,6 +342,48 @@ namespace Program1
             bool percolatesTestResult = percolatesTest();
             bool unionTestResult = unionTest();
             bool getIndexTestResult = getIndexTest();
+            string s = "";
+            int x, y;
+
+            while (s != "stop")
+            {
+                s = Console.ReadLine();
+                if (s == "init")
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    init(x);
+                }
+                if (s == "open")
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    y = Convert.ToInt32(Console.ReadLine());
+                    open(x, y);
+                }
+                if (s == "isOpen")
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(isOpen(x, y));
+                }
+                if (s == "isFull")
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(isFull(x, y));
+                }
+                if (s == "numberOfOpenSites")
+                {
+                    Console.WriteLine(numberOfOpenSites());
+                }
+                if (s == "percolates")
+                {
+                    Console.WriteLine(percolates());
+                }
+                if (s == "print")
+                {
+                    print();
+                }
+            }
         }
     }
 
