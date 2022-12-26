39bd3fb0e8867fc8d210cfd0407018ba4166adb6
DenisVaschenko <denvoser@gmail.com>
2022-12-26 10:09:24 +0200

    Prototype of the program was created

---
+++ sem_1_lab_2/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Buffers;
+using System.Data;
+using System.Transactions;
+
+namespace Program1
+{
+    class Percolation
+    {
+        // creates n-by-n grid, with all sites initially blocked
+        static void init(int n)
+        {
+        }
+
+        // opens the site (row, col) if it is not open already
+        static void open(int row, int col)
+        {
+        }
+
+        // is the site (row, col) open?
+        static bool isOpen(int row, int col)
+        {
+        }
+
+        // is the site (row, col) full?
+        static bool isFull(int row, int col)
+        {
+        }
+
+        // returns the number of open sites
+        static int numberOfOpenSites()
+        {
+        }
+
+        // does the system percolate?
+        static bool percolates()
+        {
+        }
+
+        // prints the matrix on the screen
+        // The method should display different types of sites (open, blocked, full)
+        static void print()
+        {
+        }
+        static void union(int x, int y)
+        {
+        }
+        static int getIndex(int row, int col)
+        {
+        }
+        static void Main(String[] args)
+        {
+        }
+    }
+
+}
