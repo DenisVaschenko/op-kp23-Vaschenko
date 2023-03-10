db285ceacc6ee4a68c0cc3f8c65243a50418cb9d
DenisVaschenko <denvoser@gmail.com>
2023-03-11 01:01:17 +0200

    Prototype of the program was created: Task5

---
+++ sem_2_lab_1/Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int WordsCounter(string s)
+        {
+        }
+        static bool IsLetter(char c)
+        {
+        }
+    }
+}
