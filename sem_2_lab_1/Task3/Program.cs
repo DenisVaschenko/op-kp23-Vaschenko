ad86ac20c92b10122b7f38f77c136fdb5b501058
DenisVaschenko <denvoser@gmail.com>
2023-03-11 00:50:28 +0200

    Prototype of the program was created: Task3

---
+++ sem_2_lab_1/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void CreateFile(string filepath, int maxLength, int countOfWords)
+        {
+        }
+        static bool Compare(string s1, string s2)
+        {
+        }
+        static string[] sort(string[] s)
+        {
+        }
+        static void display(string[] words)
+        {
+        }
+    }
+}
+
