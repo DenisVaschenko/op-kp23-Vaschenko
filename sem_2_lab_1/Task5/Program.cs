4ceb29299735e76df5f8bdb42ac4646d9e31f5c5
DenisVaschenko <denvoser@gmail.com>
2023-03-11 01:01:45 +0200

    Full implementation of the program was created: Task5

--- sem_2_lab_1/Task5/Program.cs
+++ sem_2_lab_1/Task5/Program.cs
@@ -4,12 +4,38 @@
     {
         static void Main(string[] args)
         {
+            string filePath = "1.txt";
+            StreamReader sr = new StreamReader(filePath);
+            string line;
+            int numOfWords = 0;
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                if (line != null) numOfWords += WordsCounter(line);
+            }
+            Console.WriteLine(numOfWords);
+            sr.Close();
         }
         static int WordsCounter(string s)
         {
+            int answer = 0;
+            if (IsLetter(s[0]))
+            {
+                answer++;
+            }
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (IsLetter(s[i]) && !IsLetter(s[i - 1]))
+                {
+                    answer++;
+                }
+            }
+            return answer;
         }
         static bool IsLetter(char c)
         {
+            if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) return true;
+            return false;
         }
     }
 }
