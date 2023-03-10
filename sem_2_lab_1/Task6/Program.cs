5965a4418ced1305802a3d9e491e2f6c62b86ec9
DenisVaschenko <denvoser@gmail.com>
2023-03-11 01:06:59 +0200

    Prototype of the program was created: Task6

---
+++ sem_2_lab_1/Task6/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        static void CreateFile(string filepath, int count)
+        {
+           
+        }
+        static string GenerateWord(int length)
+        {
+            
+        }
+        static (string, string, int) Split(string s)
+        {
+            
+        }
+    }
+}
