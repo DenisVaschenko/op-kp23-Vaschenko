e02bc99e81a90ba372907952ce98adecc2154629
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:58:52 +0300

    Full implementation of the Task2.3 was created

--- sem_2_lab_2/Task2/Program3.cs
+++ sem_2_lab_2/Task2/Program3.cs
@@ -13,24 +13,50 @@ class SailingVessel : Vessel
 {
     public void PrepareToMove()
     {
+        Console.WriteLine("Sailing Vessel is preparing to move...");
     }
     public void Move()
     {
+        Console.WriteLine("Sailing Vessel is moving");
     }
 }
 class Submarine : Vessel
 {
     public void PrepareToMove()
     {
+        Console.WriteLine("Submarine is preparing to move...");
     }
     public void Move()
     {
+        Console.WriteLine("Submarine is moving");
     }
 }
 class Program
 {
     static void Main(string[] args)
     {
+        int n;
+        Vessel vessel;
+        while (true)
+        {
+            Console.WriteLine("Enter 1 - to move Sailing Vessel; 2 - to move Submarine; 3 - to exit the program");
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n == 1) vessel = new SailingVessel();
+                else if (n == 2) vessel = new Submarine();
+                else if (n == 3) return;
+                else throw new Exception();
+                vessel.PrepareToMove();
+                vessel.Move();
+
+            }
+            catch
+            {
+                Console.WriteLine("Entered data is incorrect");
+                continue;
+            }
+        }
     }
 }
 /*
