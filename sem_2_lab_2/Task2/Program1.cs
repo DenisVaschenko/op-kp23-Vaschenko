d5b517ed38cacfc5b2dc6b14ee09b9911ed73b49
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:53:06 +0300

    Full implementation of the Task2.1 was created

--- sem_2_lab_2/Task2/Program1.cs
+++ sem_2_lab_2/Task2/Program1.cs
@@ -16,18 +16,88 @@ class Program
 
     static void Main(string[] args)
     {
+        statement = new Statement();
+        Console.WriteLine("Enter number of clients");
+        SetData(Convert.ToInt32(Console.ReadLine()));
+        GetData();
     }
     static void SetData(int count)
     {
+        string[] s;
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Enter data for " + (i + 1) + "th client");
+            s = Console.ReadLine().Split();
+            try
+            {
+                statement.AddClient(s[0], Convert.ToInt32(s[1]), Convert.ToInt32(s[2]));
+            }
+            catch
+            {
+                Console.WriteLine("Entered data is incorrect");
+                i--;
+                continue;
+            }
+            Console.WriteLine("Data is added successfully");
+        }
     }
     static void GetData()
     {
+        Console.WriteLine();
+        Console.WriteLine("N     Surname     salary     held     received");
+        for (int i = 0; i < statement.clients.Count(); i++)
+        {
+            Console.Write((i + 1) + " ");
+            int j = NumberLength(i + 1) + 2;
+            if (j < 7)
+            {
+                Print(' ', 7 - j);
+                j = 7;
+            }
+            Console.Write(statement.clients[i].surname + " ");
+            j += statement.clients[i].surname.Length + 1;
+            if (j < 19)
+            {
+                Print(' ', 19 - j);
+                j = 19;
+            }
+            Console.Write(statement.clients[i].salary + " ");
+            j += NumberLength(statement.clients[i].salary) + 1;
+            if (j < 30)
+            {
+                Print(' ', 30 - j);
+                j = 30;
+            }
+            Console.Write(statement.clients[i].held + " ");
+            j += NumberLength(statement.clients[i].held) + 1;
+            if (j < 39)
+            {
+                Print(' ', 39 - j);
+                j = 39;
+            }
+            Console.Write(statement.clients[i].received + "\n");
+        }
+        statement.CountSummary();
+        Console.WriteLine("Sum of salary: " + statement.sum1);
+        Console.WriteLine("Sum of held: " + statement.sum2);
+        Console.WriteLine("Sum of received: " + statement.sum3);
     }
     static int NumberLength(int n)
     {
+        int i = 0;
+        while (n > 0)
+        {
+            n /= 10;
+            i++;
+        }
+        return i;
     }
     static void Print(char c, int n)
     {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(c);
+        }
     }
 }
 class Client
@@ -38,6 +108,14 @@ class Client
     public int received;
     public Client(string surname, int salary, int held)
     {
+        if (salary < held)
+        {
+            throw new Exception();
+        }
+        this.surname = surname;
+        this.salary = salary;
+        this.held = held;
+        received = salary - held;
     }
 }
 class Statement
@@ -48,12 +126,21 @@ class Statement
     public int sum3;
     public void AddClient(Client client)
     {
+        clients.Add(client);
     }
     public void AddClient(string surname, int salary, int held)
     {
+        clients.Add(new Client(surname, salary, held));
     }
     public void CountSummary()
     {
+        sum1 = 0; sum2 = 0; sum3 = 0;
+        for(int i = 0; i < clients.Count(); i++)
+        {
+            sum1 += clients[i].salary;
+            sum2 += clients[i].held;
+            sum3 += clients[i].received;
+        }
     }
 }
 /*Output:
