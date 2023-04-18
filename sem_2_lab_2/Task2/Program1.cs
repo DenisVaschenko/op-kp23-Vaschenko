790f2253281e5e83165dcaf831ad4b473d14ce8a
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:51:33 +0300

    Test cases for the Task2.1 was created

--- sem_2_lab_2/Task2/Program1.cs
+++ sem_2_lab_2/Task2/Program1.cs
@@ -3,6 +3,13 @@ using System.IO.Pipes;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Transactions;
+/*
+        Input:
+        3
+        Mitchel 3000 1200
+        Locky 98800 12670
+        Surname13234 9856354 365897
+ */
 class Program
 {
     public static Statement statement;
@@ -49,3 +56,12 @@ class Statement
     {
     }
 }
+/*Output:
+        N     Surname     salary     held     received
+        1     Mitchel     3000       1200     1800
+        2     Locky       98800      12670    86130
+        3     Surname13234 9856354   365897   9490457
+        Sum of salary: 9958154
+        Sum of held: 379767
+        Sum of received: 9578387
+*/
