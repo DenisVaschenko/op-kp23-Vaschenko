7f74266adfe285e71f2c64f6631f75dd23518132
DenisVaschenko <denvoser@gmail.com>
2023-04-18 13:00:42 +0300

    Prototype of the Task3 was created

---
+++ sem_2_lab_2/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Specialized;
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+class Vector
+{
+    List<double> numbers;
+    public Vector()
+    {
+    }
+    public Vector(List<double> numbers)
+    {
+    }
+    public void AddNumber(double num)
+    {
+    }
+    public static double operator +(Vector v1, Vector v2)
+    {
+    }
+    public static double operator *(Vector v1, Vector v2)
+    {
+    }
+    public static int operator /(Vector v1, Vector v2)
+    {
+    }
+    public static Vector operator -(Vector v)
+    {
+    }
+    public static double operator -(Vector v1, Vector v2)
+    {
+    }
+}
