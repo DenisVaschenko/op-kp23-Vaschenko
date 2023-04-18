6a049c199f7734b71d7ed2d3eb21c9c215c59a85
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:46:28 +0300

    Prototype of the Task1 was created

---
+++ sem_2_lab_2/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Dynamic;
+using System.IO.Pipes;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+class Program
+{
+    public static Client[] clients;
+    static void Main(string[] args)
+    {
+    }
+    static void SetData(int count)
+    {
+    }
+    static void GetData()
+    {
+    }
+    static int NumberLength(int n)
+    {
+    }
+    static void Print(char c, int n)
+    {
+    }
+}
+class Client 
+{
+    public string surname;
+    public int salary;
+    public int held;
+    public int received;
+    public Client(string surname, int salary, int held)
+    {
+    }
+}
