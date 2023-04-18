a828bc8327f5fe92c5f3b0b90349015777725784
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:58:01 +0300

    Prototype of the Task2.3 was created

---
+++ sem_2_lab_2/Task2/Program3.cs
@@ -0,0 +1,30 @@
+﻿
+interface Vessel
+{
+    public void PrepareToMove();
+    public void Move();
+}
+class SailingVessel : Vessel
+{
+    public void PrepareToMove()
+    {
+    }
+    public void Move()
+    {
+    }
+}
+class Submarine : Vessel
+{
+    public void PrepareToMove()
+    {
+    }
+    public void Move()
+    {
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
