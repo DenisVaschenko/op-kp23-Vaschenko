d388a3891dc1b96d4f1d5757ecdb23ff8855cd4b
DenisVaschenko <denvoser@gmail.com>
2023-04-18 12:47:32 +0300

    Test cases for the Task1 was created

--- sem_2_lab_2/Task1/Program.cs
+++ sem_2_lab_2/Task1/Program.cs
@@ -3,6 +3,13 @@ using System.IO.Pipes;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Transactions;
+/*
+        Input:
+        3
+        Mitchel 3000 1200
+        Locky 98800 12670
+        Surname13234 9856354 365897
+ */
 class Program
 {
     public static Client[] clients;
@@ -24,11 +31,13 @@ class Program
 }
 class Client 
 {
-    public string surname;
-    public int salary;
-    public int held;
-    public int received;
-    public Client(string surname, int salary, int held)
-    {
-    }
 }
+/*Output:
+        N     Surname     salary     held     received
+        1     Mitchel     3000       1200     1800
+        2     Locky       98800      12670    86130
+        3     Surname13234 9856354   365897   9490457
+        Sum of salary: 9958154
+        Sum of held: 379767
+        Sum of received: 9578387
+*/
