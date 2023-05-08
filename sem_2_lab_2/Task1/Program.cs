a18d62ccc30771f5dc541da8f77779e0e7ca987b
DenisVaschenko <114067860+DenisVaschenko@users.noreply.github.com>
2023-05-08 16:53:29 +0300

    Changed type of the data(int => double)

--- sem_2_lab_2/Task1/Program.cs
+++ sem_2_lab_2/Task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Dynamic;
+using System.Dynamic;
 using System.IO.Pipes;
 using System.Numerics;
 using System.Runtime.CompilerServices;
@@ -25,11 +25,11 @@ class Program
         clients = new Client[count];
         for (int i = 0; i < count; i++)
         {
-            Console.WriteLine("Enter data for " + (i+1) + "th client");
+            Console.WriteLine("Enter data for " + (i + 1) + "th client");
             s = Console.ReadLine().Split();
             try
             {
-                clients[i] = new Client(s[0], Convert.ToInt32(s[1]), Convert.ToInt32(s[2]));
+                clients[i] = new Client(s[0], Convert.ToDouble(s[1]), Convert.ToDouble(s[2]));
             }
             catch
             {
@@ -43,53 +43,53 @@ class Program
     static void GetData()
     {
         Console.WriteLine();
-        int sum1 = 0; int sum2 = 0; int sum3 = 0;
+        double sum1 = 0; double sum2 = 0; double sum3 = 0;
         Console.WriteLine("N     Surname     salary     held     received");
         for (int i = 0; i < clients.Length; i++)
         {
             sum1 += clients[i].salary;
             sum2 += clients[i].held;
             sum3 += clients[i].received;
-            Console.Write((i+1) + " ");
+            Console.Write((i + 1) + " ");
             int j = NumberLength(i + 1) + 2;
             if (j < 7)
             {
                 Print(' ', 7 - j);
                 j = 7;
             }
-            Console.Write(clients[i].surname + " ");
+            Console.Write("{0:F2} ", clients[i].surname);
             j += clients[i].surname.Length + 1;
             if (j < 19)
             {
                 Print(' ', 19 - j);
                 j = 19;
             }
-            Console.Write(clients[i].salary + " ");
-            j += NumberLength(clients[i].salary) + 1;
+            Console.Write("{0:F2} ", clients[i].salary);
+            j += NumberLength((int)Math.Floor(clients[i].salary)) + 4;
             if (j < 30)
             {
                 Print(' ', 30 - j);
                 j = 30;
             }
-            Console.Write(clients[i].held + " ");
-            j += NumberLength(clients[i].held) + 1;
+            Console.Write("{0:F2} ", clients[i].held);
+            j += NumberLength((int)Math.Floor(clients[i].held)) + 4;
             if (j < 39)
             {
                 Print(' ', 39 - j);
                 j = 39;
             }
-            Console.Write(clients[i].received + "\n");
+            Console.Write("{0:F2} \n", clients[i].received);
         }
-        Console.WriteLine("Sum of salary: " + sum1);
-        Console.WriteLine("Sum of held: " + sum2);
-        Console.WriteLine("Sum of received: " + sum3);
+        Console.WriteLine("{1:F2}", "Sum of salary: ", sum1);
+        Console.WriteLine("{1:F2}", "Sum of held: ", sum2);
+        Console.WriteLine("{1:F2}", "Sum of received: ", sum3);
     }
     static int NumberLength(int n)
     {
         int i = 0;
         while (n > 0)
         {
-            n /= 10;
+            n = (int)n / 10;
             i++;
         }
         return i;
@@ -102,13 +102,13 @@ class Program
         }
     }
 }
-class Client 
+class Client
 {
     public string surname;
-    public int salary;
-    public int held;
-    public int received;
-    public Client(string surname, int salary, int held)
+    public double salary;
+    public double held;
+    public double received;
+    public Client(string surname, double salary, double held)
     {
         if (salary < held)
         {
@@ -119,6 +119,7 @@ class Client
         this.held = held;
         received = salary - held;
     }
+    
 }
 /*Output:
         N     Surname     salary     held     received
