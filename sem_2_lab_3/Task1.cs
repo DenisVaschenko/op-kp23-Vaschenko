7b25e5c6c195eafd718efab389e80033177aeb54
DenisVaschenko <denvoser@gmail.com>
2023-06-03 15:15:49 +0300

    Full Implementation of the task 1 was created

--- sem_2_lab_3/Task1.cs
+++ sem_2_lab_3/Task1.cs
@@ -9,27 +9,60 @@ class Deque<Item> : IIterable<Item>
     public Item[] sequence;
     public Deque()
     {
+        sequence = new Item[100];
+        head = sequence.Length;
+        tail = -1;
+        count = 0;
     }
     public bool isEmpty()
     {
+        return count == 0;
     }
     public int size()
     {
+        return count;
     }
     public void addFirst(Item item)
     {
+        count++;
+        head = head - 1;
+        if (head < 0) head = sequence.Length + head;
+        sequence[head] = item;
+        if (size() == sequence.Length) Expand();
     }
     public void addLast(Item item)
     {
+        count++;
+        tail = (tail + 1) % sequence.Length;
+        sequence[tail] = item;
+        if (size() == sequence.Length) Expand();
     }
     public Item removeFirst()
     {
+        if (isEmpty())
+        {
+            throw new InvalidOperationException();
+        }
+        Item res = sequence[head];
+        head = (head + 1) % sequence.Length;
+        count--;
+        return res;
     }
     public Item removeLast()
     {
+        if (isEmpty())
+        {
+            throw new InvalidOperationException();
+        }
+        Item res = sequence[tail];
+        tail = tail - 1;
+        if (tail < 0) tail = sequence.Length + tail;
+        count--;
+        return res;
     }
     public IIterator<Item> iterator()
     {
+        return new IteratorImpl(this);
     }
     private class IteratorImpl: IIterator<Item>
     {
@@ -39,17 +72,37 @@ class Deque<Item> : IIterable<Item>
         {
             get
             {
+                return ((counter >= deque.head && counter - deque.head < deque.size()) || (counter <= deque.tail && deque.tail - counter < deque.size()));
             }
         }
         public IteratorImpl(Deque<Item> deque)
         {
+            this.deque = deque;
+            counter = deque.head;
         }
         public Item MoveNext()
         {
+            if (!this.HasNext)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Item res = deque.sequence[counter];
+            counter = (counter + 1) % deque.sequence.Length;
+            return res;
         }
     }
     void Expand()
     {
+        Item[] temp = sequence;
+        sequence = new Item[temp.Length * 2];
+        int i = sequence.Length - size() / 2;
+        int j = head;
+        while (i >= sequence.Length - (size() / 2) || i < size()/2 + size() % 2)
+        {
+            sequence[i] = temp[j];
+            i = (i + 1) % sequence.Length;
+            j = (j + 1) % temp.Length;
+        }
     }
 }
 class DequeTest
