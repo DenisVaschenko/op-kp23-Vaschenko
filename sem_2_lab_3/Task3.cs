eec766c1bd2993a41ed17f794f15b9a76add10a7
DenisVaschenko <denvoser@gmail.com>
2023-06-03 15:26:53 +0300

    Unit testes for the task 3 were created

--- sem_2_lab_3/Task3.cs
+++ sem_2_lab_3/Task3.cs
@@ -51,24 +51,63 @@ class RandomizedQueueTest
 {
     public bool testRandomizedQueue()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        if (RandomizedQueue.sequence == null) return false;
+        return true;
     }
     public bool testIsEmpty()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        if (!RandomizedQueue.isEmpty()) return false;
+        RandomizedQueue.enque(1);
+        if (RandomizedQueue.isEmpty()) return false;
+        return true;
     }
     public bool testSize()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        if (RandomizedQueue.size() != 0) return false;
+        RandomizedQueue.enque(1);
+        if (RandomizedQueue.size() != 1) return false;
+        return true;
     }
     public bool testEnque()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        RandomizedQueue.enque(1);
+        if (RandomizedQueue.sequence[0] != 1) return false;
+        return true;
     }
     public bool testDeque()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        RandomizedQueue.enque(1);
+        RandomizedQueue.enque(2);
+        int res1 = RandomizedQueue.dequeue();
+        int res2 = RandomizedQueue.dequeue();
+        if (res1 == res2 || !(res1 == 1 || res1 == 2) || !(res2 ==1 || res2==2)) return false;
+        return true;
     }
     public bool testSample()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        RandomizedQueue.enque(1);
+        RandomizedQueue.enque(2);
+        int res1 = RandomizedQueue.sample();
+        int res2 = RandomizedQueue.sample();
+        if (!(res1 == 1 || res1 == 2) || !(res2 == 1 || res2 == 2)) return false;
+        return true;
     }
     public bool testIterator()
     {
+        RandomizedQueue<int> RandomizedQueue = new RandomizedQueue<int>();
+        RandomizedQueue.enque(1);
+        RandomizedQueue.enque(2);
+        IIterator<int> iterator = RandomizedQueue.iterator();
+        int res1 = iterator.MoveNext();
+        int res2 = iterator.MoveNext();
+        if (!(res1 == 1 || res1 == 2) || !(res2 == 1 || res2 == 2) || res1 == res2 || iterator.HasNext) return false;
+        return true;
     }
 }
 interface IIterable<T>
@@ -84,5 +123,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        RandomizedQueueTest RandomizedQueueTest = new RandomizedQueueTest();
+        if (RandomizedQueueTest.testRandomizedQueue() && RandomizedQueueTest.testIsEmpty() && RandomizedQueueTest.testSize()
+            && RandomizedQueueTest.testEnque() && RandomizedQueueTest.testDeque() && RandomizedQueueTest.testSample()
+            && RandomizedQueueTest.testIterator()) Console.WriteLine("All tests was passed");
+        else Console.WriteLine("Program work uncorrectly");
     }
 }
