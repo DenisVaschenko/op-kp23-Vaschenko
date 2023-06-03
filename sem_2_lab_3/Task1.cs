2b9bd8369ff8457f9792996e54544d3b4471ac17
DenisVaschenko <denvoser@gmail.com>
2023-06-03 15:13:24 +0300

    Prototype of the task 1 was created

---
+++ sem_2_lab_3/Task1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+
+class Deque<Item> : IIterable<Item>
+{
+    public int head;
+    public int tail;
+    int count;
+    public Item[] sequence;
+    public Deque()
+    {
+    }
+    public bool isEmpty()
+    {
+    }
+    public int size()
+    {
+    }
+    public void addFirst(Item item)
+    {
+    }
+    public void addLast(Item item)
+    {
+    }
+    public Item removeFirst()
+    {
+    }
+    public Item removeLast()
+    {
+    }
+    public IIterator<Item> iterator()
+    {
+    }
+    private class IteratorImpl: IIterator<Item>
+    {
+        Deque<Item> deque;
+        private int counter;
+        public bool HasNext
+        {
+            get
+            {
+            }
+        }
+        public IteratorImpl(Deque<Item> deque)
+        {
+        }
+        public Item MoveNext()
+        {
+        }
+    }
+    void Expand()
+    {
+    }
+}
+class DequeTest
+{
+    public bool testDeque()
+    {
+    }
+    public bool testIsEmpty()
+    {
+    }
+    public bool testSize()
+    {
+    }
+    public bool testAddFirst()
+    {
+    }
+    public bool testAddLast()
+    {
+    }
+    public bool testRemoveFirst()
+    {
+    }
+    public bool testRemoveLast()
+    {
+    }
+    public bool testIterator()
+    {
+    }
+}
+interface IIterable<T>
+{
+    public IIterator<T> iterator();
+}
+interface IIterator<T>
+{
+    bool HasNext { get; }
+    T MoveNext();
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
