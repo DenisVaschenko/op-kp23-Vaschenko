5a25e059b5606c465430251701d682cd726a6120
DenisVaschenko <denvoser@gmail.com>
2023-06-07 02:24:22 +0300

    Unit tests for the program was created

--- sem_2_lab_4/Program.cs
+++ sem_2_lab_4/Program.cs
@@ -43,21 +43,57 @@ class HashTableTest
 {
     public bool TestHashTable()
     {
+        try
+        {
+            HashTable<string, string> hashTable = new HashTable<string, string>();
+            return true;
+        }
+        catch { return false; }
     }
     public bool TestAdd()
     {
+        HashTable<string, string> hashTable = new HashTable<string, string>();
+        hashTable.Add("code1","val1");
+        return hashTable.Get("code1") == "val1";
     }
     public bool TestRemove()
     {
+        HashTable<string, string> hashTable = new HashTable<string, string>();
+        hashTable.Add("code1", "val1");
+        hashTable.Remove("code1");
+        try
+        {
+            hashTable.Get("code1");
+            return false;
+        }
+        catch { return true; }
     }
     public bool TestContains()
     {
+        HashTable<string, string> hashTable = new HashTable<string, string>();
+        hashTable.Add("code1", "val1");
+        hashTable.Add("code2", "val2");
+        hashTable.Remove("code1");
+        return hashTable.Contains("code2") && !hashTable.Contains("code1");
     }
     public bool TestClear()
     {
+        HashTable<string, string> hashTable = new HashTable<string, string>();
+        hashTable.Add("code1", "val1");
+        hashTable.Add("code2", "val2");
+        hashTable.clear();
+        return !hashTable.Contains("code2") && !hashTable.Contains("code1");
     }
     public bool TestSize()
     {
+        HashTable<string, string> hashTable = new HashTable<string, string>();
+        hashTable.Add("code1", "val1");
+        hashTable.Add("code2", "val2");
+        HashTable<string, string> hashTable2 = new HashTable<string, string>();
+        hashTable2.Add("code1", "val1");
+        hashTable2.Add("code2", "val2");
+        hashTable2.clear();
+        return hashTable.size() == 2 && hashTable2.size() == 0;
     }
 
 }
